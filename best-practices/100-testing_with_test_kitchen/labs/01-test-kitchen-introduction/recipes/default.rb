99f0d02ff94cda47e36182a4a5eed353a3a69881
GeorgeWestwater <george.westwater@progress.com>
2024-02-29 05:32:54 +0530

    update

--- best-practices/100-testing_with_test_kitchen/labs/01-test-kitchen-introduction/recipes/default.rb
+++ best-practices/100-testing_with_test_kitchen/labs/01-test-kitchen-introduction/recipes/default.rb
@@ -4,6 +4,11 @@ service 'httpd' do
   action [:enable, :start]
 end
 
-file '/var/www/html/index.html' do
-  content new_resource.homepage
+#file '/var/www/html/index.html' do
+#  content new_resource.homepage
+#end
+
+
+template '/var/www/html/index.html' do # ~FC033
+  source 'index.html.erb'
 end
