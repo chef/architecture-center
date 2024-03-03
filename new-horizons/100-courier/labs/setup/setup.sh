16611d8e5b6c093a02ee6c6b02a33432dfb583a5
GeorgeWestwater <george.westwater@progress.com>
2024-03-03 14:42:21 -0600

    changed default courier settings for lab

--- new-horizons/100-courier/labs/setup/setup.sh
+++ new-horizons/100-courier/labs/setup/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe   
 
-mkdir /home/ec2-user/nodes
+mkdir -p /home/ec2-user/nodes
 echo "ssh -i ~/.keys/key-pair.pem ec2-user@192.168.10.10" > /home/ec2-user/nodes/node-1.ssh
 echo "ssh -i ~/.keys/key-pair.pem ec2-user@192.168.10.20" > /home/ec2-user/nodes/node-2.ssh
 echo "ssh -i ~/.keys/key-pair.pem ec2-user@192.168.10.30" > /home/ec2-user/nodes/node-3.ssh
